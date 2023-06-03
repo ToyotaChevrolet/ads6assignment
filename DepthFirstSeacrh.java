31455333ace673545f38b7a342881a26f3303a07
Merey Abdikarim <124917188+ToyotaChevrolet@users.noreply.github.com>
2023-06-04 03:36:00 +0600

    Create DepthFirstSeacrh.java

---
+++ DepthFirstSeacrh.java
@@ -0,0 +1,19 @@
+
+public class DepthFirstSearch<V> extends Search<V> {
+
+    public DepthFirstSearch(WeightedGraph<V> graph, V source) {
+        super(source);
+        dfs(graph, source);
+    }
+
+    private void dfs(WeightedGraph<V> graph, V current) {
+        marked.add(current);
+        count++;
+        for(V v: graph.adjacencyList(current)) {
+            if (!marked.contains(v)) {
+                edgeTo.put(v, current);
+                dfs(graph, v);
+            }
+        }
+    }
+ }
